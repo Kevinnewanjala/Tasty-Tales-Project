03d48e8622f8a212fe8e80132c344ce9aab992ff
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-07-25 20:39:59 +0300

    Start deployment

--- server/app.py
+++ server/app.py
@@ -7,21 +7,21 @@ from flask_restful import Resource, Api  #  imported Api
 from flask_migrate import Migrate  #  imported Migrate
 
 # Local imports
-from config import app, db, api
-from authenticate import authenticate_bp, init_jwt
-from recipe import recipe_bp
+from server.config import app, db, api
+from server.authenticate import authenticate_bp, init_jwt
+from server.recipe import recipe_bp
 # Add your model imports
 from models import User, Recipe, Rating, RecipeTag, Review, Tag
 
-app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+# app = Flask(__name__)
+# app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+# # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
 
-# Initialize database and migration
-db.init_app(app)
-migrate = Migrate(app, db)
+# # Initialize database and migration
+# db.init_app(app)
+# migrate = Migrate(app, db)
 # Initialize Flask-Restful Api
 api.init_app(app)
 init_jwt(app)
